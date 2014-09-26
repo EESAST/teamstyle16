6d44e96a1e7d33b1326a7fc37a14ddce2f8c3908
Thomas Lee <lisihan969@gmail.com>
2014-09-26 18:41:23 +0800

    change name kind -> type

--- src/player_file/basic.h
+++ src/player_file/basic.h
@@ -1,6 +1,8 @@
 #ifndef TEAMSTYLE16_BASIC_H
 #define TEAMSTYLE16_BASIC_H
 
+#include <cstdint>
+
 namespace teamstyle16
 {
 
@@ -13,6 +15,8 @@ const int kMaxProductionNum = 200;  // 生产列表大小
 const int kFortScore = 1;       // 占领据点每回合奖励的积分
 const int kDamageScore = 1;     // 造成一点伤害奖励的积分
 const int kCollectScore = 1;    // 收集一单位资源奖励的积分
+
+typedef std::int_least32_t Type;
 
 enum { NO_TEAM = 2 };
 enum Level { UNDERWATER, SURFACE, AIR };  // 层次
@@ -92,7 +96,7 @@ struct State  // 状态
     int index;  // 每个元素都有唯一的索引号
 
     Position pos;  // 元素左上角的位置
-    int type;
+    Type type;
     int team;
     bool visible;
 
@@ -106,22 +110,24 @@ struct State  // 状态
 
 struct ProductionEntry
 {
-    int unit_type;
+    Type unit_type;
     int round_left;
 };
 
 struct GameInfo  // 游戏信息结构体，每回合选手从中获取必要的信息
 {
+    int x_max;
+    int y_max;
+
+    int weather;  // 天气影响所有单位的视野，数值为单位视野的改变量
+    float time_per_round;
+
     int team_num;  // 队伍号(0或1)
     int score[2];  // 两队当前积分
     int round;     // 当前总回合数
-    int weather;   // 天气影响所有单位的视野，数值为单位视野的改变量
 
     int population;  // 当前人口
     int max_polulation;  // 人口上限
-    
-    int x_max;
-    int y_max;
 
     int element_num;
     State elements[kMaxElementNum];
@@ -152,25 +158,25 @@ void ChangeDest(int operand, const Position *dest);
 void Collect(int operand, int target);
 void Explode(int operand);
 void Fix(int operand, int target);
-void Produce(int operand, int kind);
+void Produce(int operand, Type type);
 void Supply(int operand, int target, int fuel, int metal, int ammo);
 void Cancel(int operand);  // 取消回合内此前对该单位下达的 Produce 外的所有指令
 
 
 // *********************** 辅助函数 ***********************
 
-// 如果 kind 是基本类型，即取值为 [0, kElementTypes)
+// 如果 type 是基本类型，即取值为 [0, kElementTypes)
 //     返回指向 ElementType 内部的指针
-// 否则，如果 kind 可以被解析成机群，生成该机群的属性
+// 否则，如果 type 可以被解析成机群，生成该机群的属性
 //     返回值指向一个内部对象，其有效性或值可能因随后对 GetProperty 的调用而改变
-// 否则，kind 取值无效，返回 NULL
+// 否则，type 取值无效，返回 NULL
 
 const Property * GetProperty(int type);
 
-// 从类型 kind 中解析机群组成
-bool KindToFormation(int kind, Formation *formation);
-// 由机群组成中计算 kind 值
-int FormationToKind(const Formation *Formation);
+// 从类型 type 中解析机群组成
+bool TypeToFormation(int type, Formation *formation);
+// 由机群组成中计算 type 值
+int FormationToType(const Formation *Formation);
 
 
 }  // namespace teamstyle16
