66989c63e7a21a95618c204ed1f5716409aebd1f
ThomasLee <lisihan969@gmail.com>
2014-10-01 00:34:01 +0800

    Start building platform

--- src/deep_blue/platform/ai_proxy.py
+++ src/deep_blue/platform/ai_proxy.py
@@ -1,30 +1,51 @@
-"""Represent an AI"""
+import threading, time, socket
 
-class AIProxy(object):
+class AIProxy(threading.Thread):
     """Proxy for AI"""
-    def __init__(self, team_num, file_name):
-        super(AIProxy, self).__init__()
-        self.__team_num = team_num
-        self.__file_name = file_name
-        self.__team_name = None
+    def __init__(self, team_num, file_name, port_AI):
+        threading.Thread.__init__(self)
+        self.lock = threading.RLock()
+        self.team_num = team_num
+        self.file_name = file_name
+        self.commands = []
+        self.sock_AI = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.sock_AI.bind(('', port_AI))
+        self.sock_AI.listen(5)
+        self.sock_AI.settimeout(10)
+        self.stop_flag = False
 
-        self.__start_ai_file()
+        self.__run_ai()
+        self.conn, self.addr = self.sock_AI.accept()
+        self.team_name = self.__get_team_name()
 
-    def team_num(self):
-        """Getter"""
-        return self.__team_num
+    def stop(self):
+        self.stop_flag = True
 
-    def team_name(self):
-        """Getter"""
-        return self.__team_name
+    def run(self):
+        count = 0
+        while not self.stop_flag:
+            try:
+                data = self.conn.recv(1024)
+                cmd = self.__decode_commands(data.decode())
+                self.lock.acquire()
+                self.commands.extend(cmd)
+                self.lock.release()
+            except timeout:
+                continue
 
-    def get_commands(self, time_limit):
-        """Send info to AI, accept commands under the time limit"""
+    def get_commands(self):
+        """Get accepted commands"""
+        commands = self.commands[:]
+        self.lock.acquire()
+        self.commands = []
+        self.lock.release()
+        return commands
+
+    def send_info(self, battle):
+        """Send infomations to AI"""
         pass
 
-
-    def __start_ai_file(self):
-        """Start AI file, connect to it, and set the name of the team"""
+    def __run_ai(self):
         pass
 
     def __send_stable_info(self, battle):
@@ -34,3 +55,36 @@ class AIProxy(object):
     def __send_round_info(self, battle):
         """Send infos that change over rounds to the AI"""
         pass
+
+    def __decode_commands(self, data):
+        """Decode incoming data into list of commands"""
+        pass    #return [Commands]
+
+    def __encode_stable_info(self, battle):
+        """Encode stable information of battle into str"""
+        pass
+
+    def __encode_round_info(self, battle):
+        """Encode round information of battle into str"""
+        pass
+
+class AIBattle(Battle):
+    def __init__(self, map_info, ai0_filename, ai1_filename, port_AI, port):
+        self.battle = Battle(map_info)
+        self.AI_0 = AIProxy(1, ai0_filename, port_AI)
+        self.AI_1 = AIProxy(2, ai1_filename, port_AI)
+
+    def feed_ai_commands():
+        time.sleep(10)  #can be switched
+        ai0_cmds = self.AI_0.get_commands()
+        ai1_cmds = self.AI_1.get_commands()
+
+    def run_until_end():
+        try:
+            self.AI_0.start()
+            self.AI_1.start()   #I don't know how to call send_stable_info or send_round_info here
+            #if game is over:
+                #AI_0.stop()
+                #AI_1.stop()
+        except:
+            raise ...
