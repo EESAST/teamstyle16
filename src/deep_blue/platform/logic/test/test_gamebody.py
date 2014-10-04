b48e1f8c7427d60318967bbaf8ae906fc44a484d
ThomasLee <lisihan969@gmail.com>
2014-10-04 19:48:57 +0800

    Add tests for view

--- src/deep_blue/platform/logic/test/test_gamebody.py
+++ src/deep_blue/platform/logic/test/test_gamebody.py
@@ -34,34 +34,36 @@ class TestGameBody(unittest.TestCase):
         self.gamebody = gamebody.GameBody(m, ['', ''])
 
         # bases, assign ranges explicitly to avoid changes in basic.py
-        self.assertTrue(self.gamebody.map_info.add_element(
-            Base(0, Rectangle(Position(0, 0, 1),
-                              Position(2, 2, 1)), sight_ranges=[4, 10, 8],
-                                                  fire_ranges=[0, 7, 5])))
-        self.assertTrue(self.gamebody.map_info.add_element(
-            Base(1, Rectangle(Position(12, 12, 1),
-                              Position(14, 14, 1)), sight_ranges=[4, 10, 8],
-                                                    fire_ranges=[0, 7, 5])))
+        self.base0 = Base(0, Rectangle(Position(0, 0, 1),
+                                       Position(2, 2, 1)),
+                          sight_ranges=[4, 10, 8],
+                          fire_ranges=[0, 7, 5])
+        self.base1 = Base(1, Rectangle(Position(12, 12, 1),
+                                       Position(14, 14, 1)),
+                     sight_ranges=[4, 10, 8],
+                     fire_ranges=[0, 7, 5])
+        self.assertTrue(self.gamebody.map_info.add_element(self.base0))
+        self.assertTrue(self.gamebody.map_info.add_element(self.base1))
         # forts
-        self.assertTrue(self.gamebody.map_info.add_element(
-            Fort(2, Rectangle(Position(6, 6, 1),
-                              Position(7, 7, 1)))))
-        self.assertTrue(self.gamebody.map_info.add_element(
-            Fort(2, Position(9, 4, 1))))
-        self.assertTrue(self.gamebody.map_info.add_element(
-            Fort(2, Position(9, 12, 1))))
+        self.fort0 = Fort(2, Position(9, 12, 1))
+        self.fort1 = Fort(2, Rectangle(Position(6, 6, 1),
+                                       Position(7, 7, 1)))
+        self.fort2 = Fort(2, Position(9, 4, 1))
+        self.assertTrue(self.gamebody.map_info.add_element(self.fort0))
+        self.assertTrue(self.gamebody.map_info.add_element(self.fort1))
+        self.assertTrue(self.gamebody.map_info.add_element(self.fort2))
 
         # oilfield
-        self.assertTrue(self.gamebody.map_info.add_element(
-            Oilfield(Position(13, 2, 1))))
-        self.assertTrue(self.gamebody.map_info.add_element(
-            Oilfield(Position(3, 7, 1))))
+        self.oilfield0 = Oilfield(Position(13, 2, 1))
+        self.oilfield1 = Oilfield(Position(3, 7, 1))
+        self.assertTrue(self.gamebody.map_info.add_element(self.oilfield0))
+        self.assertTrue(self.gamebody.map_info.add_element(self.oilfield1))
 
         # mine
-        self.assertTrue(self.gamebody.map_info.add_element(
-            Mine(Position(14, 5, 1))))
-        self.assertTrue(self.gamebody.map_info.add_element(
-            Mine(Position(3, 10, 1))))
+        self.mine0 = Mine(Position(14, 5, 1))
+        self.mine1 = Mine(Position(3, 10, 1))
+        self.assertTrue(self.gamebody.map_info.add_element(self.mine0))
+        self.assertTrue(self.gamebody.map_info.add_element(self.mine1))
 
     def test_constants(self):
         """Test constants in module gamebody"""
@@ -73,19 +75,23 @@ class TestGameBody(unittest.TestCase):
         self.assertNotEqual(tie, 1)
         self.assertNotEqual(cont, tie)
 
-
     def test_basic_view(self):
         """Test baisc behavior of view"""
-        unit_in_sight = self.gamebody.view_elements(1)
-        print unit_in_sight
-        pass
+        units_in_sight = self.gamebody.view_elements(1)
+        for element in [self.base1, self.fort0, self.fort1, self.oilfield0,
+                        self.mine0]:
+            self.assertIn(element, units_in_sight.values())
 
     def test_view_outrange_base(self):
-        """Test """
-        pass
+        """Test behavior of viewing a base out of sight (see everything)"""
+        units_in_sight = self.gamebody.view_elements(1)
+        self.assertIn(self.base0, units_in_sight)
 
     def test_view_outrange_resource(self):
-        pass
+        """Test behavior of viewing a base out of sight (see nothing)"""
+        units_in_sight = self.gamebody.view_elements(1)
+        self.assertNotIn(self.oilfield1, units_in_sight)
+        self.assertNotIn(self.mine1, units_in_sight)
 
     def test_(self):
         self.assertTrue(self.gamebody.map_info.add_element(
