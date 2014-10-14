8e921542f2daf84d85e1799d67cc7bdc60be1c3f
ThomasLee <lisihan969@gmail.com>
2014-10-14 18:54:13 +0800

    Bug fix (var not init) in basic.py

--- src/deep_blue/platform/logic/basic.py
+++ src/deep_blue/platform/logic/basic.py
@@ -432,6 +432,7 @@ class Building(UnitBase):
         """建筑对周围单位补给"""
         result_events = []
         provides = replenishFuelAmmo(self, our_unit, fuel, ammo)
+        provide_metal = 0
         if our_unit.metal_max != None:
             provide_metal = min(metal, self.metal, our_unit.metal_max - our_unit.metal)
             self.metal -= provide_metal
@@ -555,6 +556,7 @@ class Cargo(Ship):
         """运输舰对周围单位补给燃料弹药, 可向基地, 据点, 运输舰补充金属"""
         result_events
         provides = replenishFuelAmmo(self, our_unit, fuel, ammo)
+        provide_metal = 0
         if our_unit.metal_max != None:
             provide_metal = min(metal, self.metal, our_unit.metal_max - our_unit.metal)
             self.metal -= provide_metal
