3a5ea1ee115c5f3811d93f1127225bc5de97f818
ThomasLee <lisihan969@gmail.com>
2014-10-26 23:54:50 +0800

    Add black_box_run in ai_battle

--- src/deep_blue/platform/ai_battle.py
+++ src/deep_blue/platform/ai_battle.py
@@ -3,7 +3,7 @@ import logging
 import socket
 import time
 
-from logic import gamebody
+from logic import gamebody, map_info
 import battle
 import ai_proxy
 
@@ -111,3 +111,76 @@ def load(filename, port=DEFAULT_PORT, timeout=DEFAULT_TIMEOUT,
     return AIBattle(None, port=port, timeout=timeout,
                     ai0_filename=ai0_filename, ai1_filename=ai1_filename,
                     prev_info=battle.load_prev_info(filename))
+
+def black_box_run(args):
+    """Run the game in a black box"""
+    kw = dict(ai0_filename=args.ai_0, ai1_filename=args.ai_1)
+    if args.timeout:
+        kw['timeout'] = args.timeout
+
+    if args.battle:  # Load from pervious battle
+        b = load(args.map, **kw)
+    else:  # Construct a new battle
+        m = map_info.load(args.map)
+        b = AIBattle(m, **kw)
+
+    if args.time_per_round:  # override time_per_round of the map
+        b.gamebody.map_info.time_per_round = args.time_per_round
+
+    b.run_until_end()
+
+    # Print result
+    print b.round(), 'round(s) passed'
+    result = b.state()
+    if result == 0 or result == 1:
+        print 'Team %d wins' % result
+    elif result == gamebody.STATE_TIE:
+        print 'Tie (%d : %d)' % (b.score(0), b.score(1))
+    else:
+        raise RuntimeError('Invalid game state: %d', result)
+
+    # Save into files
+    if args.save:
+        b.save('hehe.battle', compact=args.compact, compress=args.compress)
+    if args.out:
+        b.save_event_strings(args.out)
+
+    del b  # Destroy the battle
+
+
+if __name__ == '__main__':
+    import argparse
+    parser = argparse.ArgumentParser()
+    parser.add_argument('map', help='map file')
+    parser.add_argument('ai_0', help='file name of AI 0')
+    parser.add_argument('ai_1', help='file name of AI 1')
+    parser.add_argument('-s', '--save', metavar='<file>',
+                        help='Save the game to <file> after it ends')
+    parser.add_argument('-o', '--out', metavar='<file>',
+                        help='Place the output into <file>')
+    parser.add_argument('--timeout', type=float,
+                        help='Set timeout when waiting for AI connection')
+    parser.add_argument('-t', '--time_per_round', type=float,
+                        help='Override time interval of the map')
+    parser.add_argument('-b', '--battle', action='store_true',
+                        help='Treat map file as a battle')
+    parser.add_argument('--compact', action='store_true',
+                        help='Make save file more compact')
+    parser.add_argument('--compress', action='store_true',
+                        help='Compress save file')
+
+    parser.add_argument('-v', '--verbose', action='count',
+                        help='increase output verbosity')
+
+    args = parser.parse_args()
+
+    v = args.verbose
+    root = logging.getLogger()
+    if not v:
+        root.setLevel(logging.WARNING)
+    elif v == 1:
+        root.setLevel(logging.INFO)
+    else:
+        root.setLevel(logging.DEBUG)
+
+    black_box_run(args)
