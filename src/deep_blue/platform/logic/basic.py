649775670e1bc1e8799a8edad752eec846dd8e31
ThomasLee <lisihan969@gmail.com>
2015-02-21 13:52:57 +0800

    Fix a syntax error, please review @Nuullll

--- src/deep_blue/platform/logic/basic.py
+++ src/deep_blue/platform/logic/basic.py
@@ -561,7 +561,7 @@ class Unit(UnitBase):
                     collect_event = self.collect(near_element)
                     events += collect_event
                     game.scores[self.team] += (max(collect_event[0].fuel, collect_event[0].metal) * COLLECT_SCORE
-                        if len(collect_event) != 0)
+                        if len(collect_event) != 0 else 0)
 
         # fuel
         if isinstance(self, Plane):
