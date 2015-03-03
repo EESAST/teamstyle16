267bbbfc810199cc90eb3274485f54b8f929a754
Vone <vfirst218@gmail.com>
2015-03-03 23:22:19 +0800

    Fix syntax error

--- src/deep_blue/platform/logic/basic.py
+++ src/deep_blue/platform/logic/basic.py
@@ -159,13 +159,13 @@ PROPERTY = [OBJ[NAMES[i]] for i in range(10)]
 
 # cannot be changed
 CONSTANT = {
-    BASE: ['ammo_max', 'build_round', 'cost', 'population', 'speed']
-    CARGO: ['ammo_once', 'attacks', 'fire_ranges']
-    CARRIER: ['metal_max']
-    DESTROYER: ['metal_max']
-    FIGHTER: ['metal_max']
-    FORT: ['build_round', 'cost', 'population', 'speed']
-    SCOUT: ['metal_max']
+    BASE: ['ammo_max', 'build_round', 'cost', 'population', 'speed'],
+    CARGO: ['ammo_once', 'attacks', 'fire_ranges'],
+    CARRIER: ['metal_max'],
+    DESTROYER: ['metal_max'],
+    FIGHTER: ['metal_max'],
+    FORT: ['build_round', 'cost', 'population', 'speed'],
+    SCOUT: ['metal_max'],
     SUBMARINE: ['metal_max']
     }
 
