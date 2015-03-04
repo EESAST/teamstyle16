cbf95dc1b367a47de51ac4fa167bb3645c0e4971
ThomasLee <lisihan969@gmail.com>
2015-03-04 21:28:01 +0800

    呵呵

--- src/deep_blue/platform/logic/command.py
+++ src/deep_blue/platform/logic/command.py
@@ -63,7 +63,7 @@ class AttackUnit(Command):
         elements = game.map_info.elements
         attacker = elements.get(self.operand)
         defender = elements.get(self.target)
-        if attacker == None or defender == None or attacker.kind == CARRIER or attacker.kind == CARGO:
+        if attacker == None or defender == None or attacker.kind == CARGO:
             return False
         if hasattr(defender, 'team') is False or defender.team == attacker.team:
             return False
