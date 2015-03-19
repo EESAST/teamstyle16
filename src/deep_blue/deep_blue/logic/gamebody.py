3bcb8694e72794c6d555b962025fab135fa7e79d
Vone <vfirst218@gmail.com>
2015-03-19 14:41:51 +0800

    Fix bug in weather affection calculate

--- src/deep_blue/deep_blue/logic/gamebody.py
+++ src/deep_blue/deep_blue/logic/gamebody.py
@@ -110,7 +110,8 @@ class GameBody(object):
         vision = [[], [], []]
         for level in xrange(3):
             for element in self.elements(team).values():
-                vision[level].extend(element.pos.region(level, element.sight_ranges[level] + self.map_info.weather))
+                vision[level].extend(element.pos.region(level, 
+                    max(0, element.sight_ranges[level] + self.map_info.weather)))
             vision[level] = set(vision[level])
             tmp = []
             for point in vision[level]:
