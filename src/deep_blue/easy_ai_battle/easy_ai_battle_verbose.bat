ec6471da8e1d86c35437424062b223aea2b7e6d4
ThomasLee <lisihan969@gmail.com>
2015-03-20 14:35:11 +0800

    Use ai0 & ai1 instead of 1 & 2

--- src/deep_blue/easy_ai_battle/easy_ai_battle_verbose.bat
+++ src/deep_blue/easy_ai_battle/easy_ai_battle_verbose.bat
@@ -1,4 +1,4 @@
 cd %~dp0
 cd ..
-ai_battle.exe -s easy_ai_battle\replay.battle easy_ai_battle\1.map easy_ai_battle\ai1.exe easy_ai_battle\ai2.exe -vvv
+ai_battle.exe -s easy_ai_battle\replay.battle easy_ai_battle\1.map easy_ai_battle\ai0.exe easy_ai_battle\ai1.exe -vvv
 pause
