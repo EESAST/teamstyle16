ad11da256b13cf4e0aeb886ea0a96baa053378ed
ThomasLee <lisihan969@gmail.com>
2015-03-20 14:12:02 +0800

    Add easy_ai_battle, modify easy behavior

--- src/deep_blue/easy_debugger/easy_debugger_verbose.bat
+++ src/deep_blue/easy_debugger/easy_debugger_verbose.bat
@@ -1,4 +1,4 @@
 cd %~dp0
 cd ..
-debugger.exe -s easy_debugger\replay.battle test_match_1.map ai\empty_ai.exe -vvv
+debugger.exe -s easy_debugger\replay.battle 1.map easy_debugger/ai1.exe -vvv
 pause
