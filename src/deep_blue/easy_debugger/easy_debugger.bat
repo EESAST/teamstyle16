79e15feb966961c10de4829ba0a98be71c1b8d9a
ThomasLee <lisihan969@gmail.com>
2015-03-20 13:43:16 +0800

    Put easy_debuggers into a dir

---
+++ src/deep_blue/easy_debugger/easy_debugger.bat
@@ -0,0 +1,4 @@
+cd %~dp0
+cd ..
+debugger.exe -s easy_debugger\replay.battle test_match_1.map ai\empty_ai.exe
+pause
--- src/deep_blue/easy_debugger_verbose.bat
+++
@@ -1,3 +0,0 @@
-cd %~dp0
-debugger.exe -s replay.battle test_match_1.map ai\empty_ai.exe -vvv
-pause
